0fa61da48bfbd6f145d60461d44077271fea8e63
govinddixit <govind.dixit@cred.club>
2021-10-05 23:42:34 +0530

    refactor package name to dsc.iiitl.app

---
+++ app/src/main/java/dsc/iiitl/app/activities/AboutPageActivity.java
@@ -0,0 +1,121 @@
+package dsc.iiitl.app.activities;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import dsc.iiitl.app.R;
+
+public class AboutPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout fork;
+    private LinearLayout developers;
+    private LinearLayout rateApp;
+    private LinearLayout sendArticle;
+    private ImageView dsc_about;
+    private TextView versionTextView;
+
+    String versionName = "1.0";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("About");
+        setContentView(R.layout.activity_about_page);
+        fork = findViewById(R.id.Star_on_github);
+        developers = findViewById(R.id.developers);
+        rateApp = findViewById(R.id.rate_the_app);
+        dsc_about = findViewById(R.id.dsc_about);
+        sendArticle = findViewById(R.id.Submit_article);
+        versionTextView = findViewById(R.id.app_version);
+        fork.setOnClickListener(this);
+        developers.setOnClickListener(this);
+        rateApp.setOnClickListener(this);
+        sendArticle.setOnClickListener(this);
+        dsc_about.setOnClickListener(this);
+
+        try {
+            versionName = getApplicationContext().getPackageManager()
+                    .getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        versionTextView.setText(versionName);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.Star_on_github:
+                chromeCustomTabs(getResources().getString(R.string.Star_on_github));
+                break;
+            case R.id.developers:
+                chromeCustomTabs(getResources().getString(R.string.developers_github_link));
+                break;
+            case R.id.rate_the_app:
+                rateApp();
+                break;
+            case R.id.Submit_article:
+                sendArticle();
+                break;
+            case R.id.dsc_about:
+                chromeCustomTabs(getResources().getString(R.string.dsc_website));
+                break;
+            default:
+                break;
+        }
+    }
+
+//    private void sendMail() {
+//        String mailto = "mailto:dsc@iiitl.ac.in?subject=Feedback Submission";
+//
+//        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+//        emailIntent.setData(Uri.parse(mailto));
+//        try {
+//            startActivity(emailIntent);
+//        } catch (ActivityNotFoundException e) {
+//            //TODO: Handle case where no email app is available
+//        }
+//    }
+
+    private void sendArticle() {
+        String mailto = "mailto:dsc@iiitl.ac.in?subject=Article submission for Student App";
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse(mailto));
+        try {
+            startActivity(emailIntent);
+        } catch (ActivityNotFoundException e) {
+            //TODO: Handle case where no email app is available
+        }
+    }
+
+    private void chromeCustomTabs(String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(Color.parseColor("#000000"));
+        CustomTabsIntent intent = builder.build();
+        intent.launchUrl(this, Uri.parse(url));
+    }
+
+    private void rateApp() {
+        final String appPackageName = getPackageName();
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+    }
+}
---
+++ app/src/main/java/dsc/iiitl/app/activities/AskDetailActivity.java
@@ -0,0 +1,140 @@
+package dsc.iiitl.app.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dsc.iiitl.app.R;
+
+public class AskDetailActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner yearone ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_detail);
+
+        Spinner branch = findViewById(R.id.branch);
+        ArrayAdapter<CharSequence> yearadapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.branch, R.layout.spinner_item);
+        yearadapter.setDropDownViewResource(R.layout.spinner_item);
+        branch.setAdapter(yearadapter);
+        branch.setOnItemSelectedListener(this);
+
+        yearone = findViewById(R.id.year);
+
+        Button button = findViewById(R.id.ok_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String conc = null;
+                String course = branch.getSelectedItem().toString();
+                String yearse = yearone.getSelectedItem().toString();
+
+                if(course.equals("CSE (B.Tech)")){
+                    switch (yearse) {
+                        case "1":
+                            conc = "cs1sem.json";
+                            break;
+//                        case "2":
+//                            conc = "cs2sem.json";
+//                            break;
+//                        case "3":
+//                            conc = "cs3sem.json";
+//                            break;
+//                        case "4":
+//                            conc = "cs4sem.json";
+//                            break;
+//                        case "5":
+//                            conc = "cs5sem.json";
+//                            break;
+//                        case "6":
+//                            conc = "cs6sem.json";
+//                            break;
+//                        case "7":
+//                            conc = "cs7sem.json";
+//                            break;
+                        default:
+                            conc = "cs2sem.json";
+                            break;
+                    }
+                }else if(course.equals("IT (B.Tech)")){
+                    switch (yearse) {
+                        case "1":
+                            conc = "it1sem.json";
+                            break;
+                        case "2":
+                            conc = "it2sem.json";
+                            break;
+                        case "3":
+                            conc = "it3sem.json";
+                            break;
+                        case "4":
+                            conc = "it4sem.json";
+                            break;
+                        case "5":
+                            conc = "it5sem.json";
+                            break;
+                        case "6":
+                            conc = "it6sem.json";
+                            break;
+                        case "7":
+                            conc = "it7sem.json";
+                            break;
+                        default:
+                            conc = "it8sem.json";
+                            break;
+                    }
+                }else {
+                    switch (yearse) {
+                        case "1":
+                            conc = "mcs1sem.json";
+                            break;
+//                        case "2":
+//                            conc = "cs2sem.json";
+//                            break;
+//                        case "3":
+//                            conc = "cs3sem.json";
+//                            break;
+                        default:
+                            conc = "mcs2sem.json";
+                            break;
+                    }
+                }
+
+                Intent intent = new Intent(AskDetailActivity.this, TimeTableActivity.class);
+                intent.putExtra("cs2sem.json",conc);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        String branchselect = adapterView.getItemAtPosition(i).toString();
+        if ("CSE (M.Tech)".equals(branchselect) || "CSE (B.Tech)".equals(branchselect)) {
+            Spinner yearone = findViewById(R.id.year);
+            ArrayAdapter<CharSequence> yearadapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.mtech, R.layout.spinner_item);
+            yearadapter.setDropDownViewResource(R.layout.spinner_item);
+            yearone.setAdapter(yearadapter);
+        } else {
+
+            ArrayAdapter<CharSequence> yearadapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.year, R.layout.spinner_item);
+            yearadapter.setDropDownViewResource(R.layout.spinner_item);
+            yearone.setAdapter(yearadapter);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        /*
+        Todo on nothing selected
+         */
+    }
+}
